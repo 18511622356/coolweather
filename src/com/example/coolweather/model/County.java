b17b7da6c973855f428980aa9390211f91df2a20
yan <yanjianning@qq.com>
2016-08-01 23:57:56 +0800

    新增数据库帮助类，以及各表对应的实体类。

---
+++ src/com/example/coolweather/model/County.java
@@ -0,0 +1,43 @@
+package com.example.coolweather.model;
+
+import android.R.integer;
+
+public class County {
+	private int id;
+	private String countyName;
+	private String countyCode;
+	private int cityId;
+	
+	public int getId(){
+		return id;
+	}
+	
+	public void setId(int id){
+		this.id = id;
+	}
+	
+	public String getcountyName(){
+		return countyName;
+	}
+	
+	public void setcountyName(String countyName){
+		this.countyName = countyName;
+	}
+	
+	public String getcountyCode(){
+		return countyCode;
+	}
+	
+	public void setcountyCode(String countyCode){
+		this.countyCode = countyCode;
+	}
+	
+	public int getCityId(){
+		return cityId;
+	}
+	
+	public void setCityId(int cityId){
+		this.cityId = cityId;
+	}
+
+}
